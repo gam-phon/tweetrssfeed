eda212f1a7b73e7e66267d03a8b78594f23d5812
Rehab Alhejaily <rehab@Rehabs-MacBook-Air.local>
2015-01-09 14:35:35 +0300

    let test pass

--- tests/test_tweetrssfeed.py
+++ tests/test_tweetrssfeed.py
@@ -10,23 +10,6 @@ Tests for `tweetrssfeed` module.
 
 import unittest
 
-from tweetrssfeed import twitter_connection, bitly_connection
-
-
-def test_twitter(api):
-    ''' Test Twitter '''
-    twitter_name = api.me().name
-    assert twitter_name is not None
-    print(twitter_name)
-
-
-def test_bitly(api):
-    data = api.shorten('http://google.com/')
-    assert data is not None
-    assert data['long_url'] == 'http://google.com/'
-    assert data['hash'] is not None
-    print(data['url'])
-
 
 class TestTweetrssfeed(unittest.TestCase):
 
@@ -41,10 +24,3 @@ class TestTweetrssfeed(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-    # Access
-    api_twitter = twitter_connection()
-    api_bitly = bitly_connection()
-
-    #Test
-    test_twitter(api_twitter)
-    test_bitly(api_bitly)
